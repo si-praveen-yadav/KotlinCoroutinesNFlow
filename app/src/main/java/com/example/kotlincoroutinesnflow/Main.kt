30c44eb8b7f8adf3442d91146eb72931e0f35718
Praveen Yadav <praveen.yadav@sportzinteractive.net>
2023-08-23 19:37:16 +0530

    functions in kotlin

--- app/src/main/java/com/example/kotlincoroutinesnflow/Main.kt
+++ app/src/main/java/com/example/kotlincoroutinesnflow/Main.kt
@@ -10,45 +10,6 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlin.system.measureTimeMillis
 
-fun main() {
-//    print(testCoroutines())
-    /*runBlocking {
-        launch(Dispatchers.Default) {
-            asyncOperation()       // this is running in bg thread
-            launch(Dispatchers.IO) {
-                completionHandler() // this is running in main thread.
-            }
-        }
-    }*/
-    /*runBlocking {
-        printHelloWorldWithRunBlocking()
-        println("Done")
-    }*/
-
-/*
-    runBlocking {
-        printHelloWorldWithCoroutineScope()
-        println("Done")
-    }
-
-*/
-
-    /*
-        GlobalScope.launch {
-            printHelloWorldWithCoroutineScope()
-        }
-        println("DONE")
-
-        GlobalScope.launch {
-            printHelloWorldWithCoroutineScope()
-        }
-        println("DONE")
-    */
-
-//    print1To100()
-//    print1To100WithoutCoroutineScope()
-    testWaitForLaunchJob()
-}
 
 fun testCoroutines()=measureTimeMillis {
     runBlocking {
@@ -169,7 +130,152 @@ fun testWaitForLaunchJob(){
         println("Done")
     }
 
-    // Higher Order functions
+}
 
-    
+// Higher Order functions
+
+//    If the last argument after default parameters is a lambda, you can pass it either as a named argument or outside the parentheses:
+fun foo(
+    bar: Int = 0,
+    baz: Int = 1,
+    qux: () -> Unit,
+) {
+    println("Inside foo method")
+    println("bar = $bar")
+    println("baz = $baz")
+    println("qux = $qux")
+}
+
+//You can pass a variable number of arguments (vararg) with names using the spread operator:
+fun concatStrings(vararg args:String,seperator:String = " "):String{
+    var output = ""
+    args.forEachIndexed { index, s ->
+        if(index<args.size-1){
+            output += s+seperator
+        }else{
+            output += s
+        }
+    }
+
+    return output
+}
+
+//Single-expression functions
+//When the function body consists of a single expression, the curly braces can be omitted and the body specified after an = symbol
+
+fun square(x: Int): Int = x * x
+fun triple(x: Int) = x * 3
+
+fun allVariant1(type:String,vararg colors:String){
+    colors.forEach {
+        println("$it $type")
+    }
+}
+
+fun allVariant2(vararg colors:String,type:String){
+    colors.forEach {
+        println("$it $type")
+    }
+}
+
+/*
+* Infix notation
+Functions marked with the infix keyword can also be called using the infix notation (omitting the dot and the parentheses for the call).
+* Infix functions must meet the following requirements:
+
+They must be member functions or extension functions.
+
+They must have a single parameter.
+
+The parameter must not accept variable number of arguments and must have no default value.
+* */
+
+infix fun Int.plus(b:Int)= this+b
+
+///Functions can have generic parameters, which are specified using angle brackets before the function name:
+data class Student(
+    val firstName : String,
+    val lastName : String,
+    val title:String,
+)
+
+data class Professor(
+    val firstName : String,
+    val lastName : String,
+    val title:String,
+    val position:String
+)
+
+fun <T> getDisplayName(item: T): String {
+    return when(item){
+        is Student-> "${item.title} ${item.firstName} ${item.lastName}"
+        is Professor->"${item.title} ${item.firstName} ${item.lastName} (${item.position})"
+        else->""
+    }
+}
+fun main() {
+    val student = Student("Praveen","Yadav","Mr.")
+    val professor = Professor("Manoj","Kumar","Dr.","Professor,MCA")
+
+    println(getDisplayName(student))
+    println(getDisplayName(professor))
+    // Two ways to call infix function
+/*
+    println(1.plus(4))
+    print(1 plus 4)
+*/
+
+    /*allVariant1("Shirt","Red","Blue","Yellow")
+    //allVariant2("Shirt","Red","Blue","Yellow")  // error no value passed for type
+    // to all allVariant2 method we have two ways
+
+    allVariant2(*arrayOf("Red","Blue","Yellow"), type = "Shirt")
+    allVariant2("Red","Blue","Yellow", type = "Shirt")*/
+    /*println(square(5))
+    println(triple(5))*/
+//    print(concatStrings("I","am","mobile","app","developer"))
+    /*// Method 1
+    foo() { println("hello") }     // Uses the default value baz = 1
+    // Method 2
+    foo(qux = { println("hello") }) // Uses both default values bar = 0 and baz = 1
+    // Method 3
+    foo { println("hello") }
+*/
+//    print(testCoroutines())
+    /*runBlocking {
+        launch(Dispatchers.Default) {
+            asyncOperation()       // this is running in bg thread
+            launch(Dispatchers.IO) {
+                completionHandler() // this is running in main thread.
+            }
+        }
+    }*/
+    /*runBlocking {
+        printHelloWorldWithRunBlocking()
+        println("Done")
+    }*/
+
+    /*
+        runBlocking {
+            printHelloWorldWithCoroutineScope()
+            println("Done")
+        }
+
+    */
+
+    /*
+        GlobalScope.launch {
+            printHelloWorldWithCoroutineScope()
+        }
+        println("DONE")
+
+        GlobalScope.launch {
+            printHelloWorldWithCoroutineScope()
+        }
+        println("DONE")
+    */
+
+//    print1To100()
+//    print1To100WithoutCoroutineScope()
+//    testWaitForLaunchJob()
 }
